a2f14bf82fbb77db1a790b31e0b15e217a4feb90
Bino26 <hbino1999@gmail.com>
2023-12-13 21:48:26 +0000

    Fixed Deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); //cross the app
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(favicon(__dirname + "/public/favicon.ico"));
 
