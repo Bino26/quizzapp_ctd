6c0c9d64114816eca86c94024643a4012d2eda02
Bino26 <hbino1999@gmail.com>
2024-01-03 00:14:28 +0000

    Update BASE_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 export let BASE_URL;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set production API URL
-  BASE_URL = 'https://jsquiz-u1ri.onrender.com/api/v1';
+  BASE_URL = "https://quizz-test.onrender.com/api/v1";
 } else {
   // Set local development API URL
-  BASE_URL = 'http://localhost:8000/api/v1';
+  BASE_URL = "http://localhost:8000/api/v1";
 }
